7be82804239e66f81a375c0885d80a55aa15c298
aman-agrawala <amanagrawala@gmail.com>
2017-05-10 16:31:11 -0400

    feat: Complete Manhattan priority function.

--- Board.java
+++ Board.java
@@ -33,12 +33,26 @@ public class Board {
         int dim = dimensions();
         int manhattan = 0;
         int val = 0;
+        int r = 0;
+        int c = 0;
         for(int i = 0; i < dim; i++)
         {
             for(int j = 0; j < dim; j++)
             {
-                val = board[i][j];
-                manhattan += ()
+                if(board[i][j] != 0)
+                {
+                    val = board[i][j];
+                    c = (val%dim)-1;
+                    r = val/dim;
+
+                    if(c<0)
+                    {
+                        c = dim;
+                        r -= 1;
+                    }
+
+                    manhattan += (Math.abs(r-i)+Math.ads(c-j));
+                } 
             }
         }
     }
