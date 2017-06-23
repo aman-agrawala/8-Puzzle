2992fbc75add66b99264be33887fbccabd76fdd6
Aman Agrawala <amanagrawala@gmail.com>
2017-06-23 11:28:35 -0400

    feat: Add neighbor boards and toString functions.

--- Board.java
+++ Board.java
@@ -1,9 +1,16 @@
+//import java.util.Stack;
+import edu.princeton.cs.algs4.Stack;
+
 public class Board {
     int[][] board;
+    int zeroR;
+    int zeroC;
     
     public Board(int[][] blocks)
     {
         board = blocks;
+        zeroR = -1;
+        zeroC = -1;
     }
     
     public int dimensions()
@@ -22,6 +29,12 @@ public class Board {
                 if(board[i][j] != (j+i+1) && board[i][j] != 0)
                 {
                     hamming+= 1; 
+                }
+                
+                if(board[i][j] == 0)
+                {
+                    zeroR = i;
+                    zeroC = j;
                 }
             }
         }   
@@ -53,6 +66,12 @@ public class Board {
 
                     manhattan += (Math.abs(r-i))+(Math.abs(c-j));
                 } 
+                
+                if(board[i][j] == 0)
+                {
+                    zeroR = i;
+                    zeroC = j;
+                }
             }
         }
         return manhattan;
@@ -96,6 +115,75 @@ public class Board {
     
     public Iterable<Board> neighbors()
     {
+        Stack<Board> neighbors = new Stack<Board>();
+        int[][] neighbor = board;
+        if(zeroR == -1 && zeroC == -1)
+        {
+            hamming();
+        }
         
+        for(int i = 0; i<4; i++)
+        {
+            int zeroRMove = 0;
+            int zeroCMove = 0;
+            
+            if(i == 0) // top
+            {
+                zeroRMove = -1;
+                zeroCMove = 0;
+            }
+            else if(i == 1) // right
+            {
+                zeroRMove = 0;
+                zeroCMove = 1;
+            }
+            else if(i == 2) // bottom
+            {
+                zeroRMove = 1;
+                zeroCMove = 0;
+            }
+            else if(i == 3) // left
+            {
+                zeroRMove = 0;
+                zeroCMove = -1;
+            }
+            
+            try // first try the top most point
+            {
+                neighbor = board;
+                int temp = neighbor[zeroR][zeroC];
+                neighbor[zeroR][zeroC] = neighbor[zeroR+zeroRMove][zeroC+zeroCMove];
+                neighbor[zeroR+zeroRMove][zeroC+zeroCMove] = temp;
+                neighbors.push(new Board(neighbor));
+            }
+            catch (ArrayIndexOutOfBoundsException E)
+            {
+                System.out.println("No " + i + " value");
+            }
+        }
+        return neighbors;
+    }
+    
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder();
+        int n = dimensions();
+        s.append(dimensions() + "\n");
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                s.append(String.format("%2d ", board[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+    
+    public static void main(String[] args)
+    {
+        int[][] blocks = new int[3][3];
+        
+        Board test = new Board(blocks);
     }
 }
